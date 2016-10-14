426ea6346ac82e064731cc23038da088b8b06b8d
WhoseSomebody <whosesomebody@gmail.com>
2016-10-14 12:20:22 +0300

    live front v1

--- public/javascripts/common.js
+++ public/javascripts/common.js
@@ -1,21 +1,38 @@
-function focusLink(isOnFocus)
-{
-    if (isOnFocus)
-      document.getElementById('formspace').className = "pass-phrase focused";
-    else
-      document.getElementById('formspace').className = "pass-phrase";
-}
 
-function toggle_pass(){
-    var pass = document.getElementsByClassName('showhide')[0].innerHTML;
-        if(pass=="show")
-         {
-            document.getElementsByClassName('password')[0].type = "text";
-            pass= "Hide";
-         }
-        else
-         {
-            document.getElementsByClassName('password')[0].type = "password";
-            pass = "Show";
-         }
+$( document ).ready(function(){
+    $('textarea.password').bind('input propertychange', function() {
+
+          // $("#yourBtnID").hide();
+
+          if(this.value.length){
+            if ($('.show').hasClass('hidden'))
+                $('.show').removeClass('hidden');
+                $('.paste, .file').addClass('hidden');
+
+          } else {
+                $('.show').addClass('hidden');
+                $('.paste, .file').removeClass('hidden');
+          }
+    });
+
+    $('.save-to-file').click(function() {
+        contentType =  'data:application/octet-stream,';
+        var area = $(this.closest(".pass-phrase")).find("textarea");
+        uriContent = contentType + encodeURIComponent(area.val());
+        this.setAttribute('href', uriContent);
+        var filename = "key.txt";
+        $("<a download='" + filename + "' href='" + uriContent + "'></a>")[0].click();
+    });
+})
+
+function onFileSelected(event, me) {
+  var selectedFile = event.target.files[0];
+  var reader = new FileReader();
+  var result = $(me.closest(".pass-phrase")).find("textarea")
+  
+  reader.onload = function(event) {
+    result.text(event.target.result.replace(/(\r\n|\n|\r)/gm,"").substring(0, 69));
+  };
+
+  reader.readAsText(selectedFile);
 }
