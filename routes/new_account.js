c360c8c6b04bc070cf40a7337672f65c9052dd01
WhoseSomebody <whosesomebody@gmail.com>
2016-10-17 13:14:59 +0300

    testng

--- routes/new_account.js
+++ routes/new_account.js
@@ -6,6 +6,13 @@ router.get('/', function(req, res, next) {
 	if(req.session.userID)
   		res.redirect('/account');
   	else{ 
+  		 console.log(platform.os.family);
+
+  		if (platform.os.family != undefined)
+  		    if (platform.os.family.indexOf("iOS") >= 0 || platform.os.family.indexOf("Mac") >= 0)
+  		     {req.session.os = "X_X"} else {req.session.os = "ok"}
+  		console.log(req.session.os);
+  		
   		if (req.session.os == "X_X")
   			res.render('new_account', { title: 'Sign Up | Nodio Crowd', buttonTextB: "Go to the Next Step", buttonTextS: "NEXT" });
   		else if (req.session.os == "ok")
