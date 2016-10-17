2c0b283e0bc6f698d84a5440f14c6d91011c6dfc
WhoseSomebody <whosesomebody@gmail.com>
2016-10-17 13:30:26 +0300

    testng

--- routes/new_account.js
+++ routes/new_account.js
@@ -1,5 +1,6 @@
 var express = require('express');
-var router = express.Router();
+var router = express.Router(),
+	platform = require('platform');
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
@@ -15,7 +16,7 @@ router.get('/', function(req, res, next) {
 
   		if (req.session.os == "X_X")
   			res.render('new_account', { title: 'Sign Up | Nodio Crowd', buttonTextB: "Go to the Next Step", buttonTextS: "NEXT" });
-  		else (req.session.os == "ok")
+  		else if (req.session.os == "ok")
   			res.render('new_account', { title: 'Sign Up | Nodio Crowd', buttonTextB: "Copy Pass Phrase to Clipboard", buttonTextS: "COPY" });
   	}
   
